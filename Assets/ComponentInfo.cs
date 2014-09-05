31a83fe06e1cfd52dc679355958beb614cbfa1b4
Alican Şekerefe <asekerefe@gmail.com>
2014-09-05 19:28:52 +0300

    internal component info structure moved to a separate class.

---
+++ Assets/ComponentInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using UnityEngine;
+
+namespace RuntimeScriptInspector
+{
+    public struct ComponentInfo
+    {
+        public MonoBehaviour component;
+        public bool visible;
+        public List<MemberInfo> fields;
+        public List<MemberInfo> methods;
+        public int guiLength;
+
+        public ComponentInfo(MonoBehaviour component)
+        {
+            this.component = component;
+            this.fields = new List<MemberInfo>();
+            this.methods = new List<MemberInfo>();
+            this.visible = true;
+            this.guiLength = 0;
+        }
+    }
+}
--- Assets/RuntimeScriptInspector.cs
+++ Assets/RuntimeScriptInspector.cs
@@ -239,22 +239,4 @@ namespace RuntimeScriptInspector
             }
         }
     }
-
-    internal struct ComponentInfo
-    {
-        public MonoBehaviour component;
-        public bool visible;
-        public List<MemberInfo> fields;
-        public List<MemberInfo> methods;
-        public int guiLength;
-
-        public ComponentInfo(MonoBehaviour component)
-        {
-            this.component = component;
-            this.fields = new List<MemberInfo>();
-            this.methods = new List<MemberInfo>();
-            this.visible = true;
-            this.guiLength = 0;
-        }
-    }
 }
